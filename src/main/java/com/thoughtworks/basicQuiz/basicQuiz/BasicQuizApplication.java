979e8f0c79960e324ffcb276a893e2be7e45e8ac
lemon-piepie <purplechilde@163.com>
2020-09-15 11:08:45 +0800

    init program back

---
+++ src/main/java/com/thoughtworks/basicQuiz/basicQuiz/BasicQuizApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.basicQuiz.basicQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/basicQuiz/basicQuiz/BasicQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.basicQuiz.basicQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
